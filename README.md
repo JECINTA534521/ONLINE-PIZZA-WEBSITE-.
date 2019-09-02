1dcccd9af23469208d0b007557a1fc96c7a9d27c
Jecinta wanjiru <jecintawanjirug@gmail.com>
2019-09-02 15:12:29 +0300

    this adds option selected values in the html form

---
+++ README.md
@@ -0,0 +1,3 @@
+#### ONLINE PIZZA WEBSITE.
+#### DESCRIPTION.
+#### This project is about pizza ordering. It helps the user to order the size of pizza they want,the type of crust they want on their pizza, and the type of topping they want. Also helps the user to choose whether they want it delivered or not.Also shows the price they will pay for ordering and also the delivery fee.
--- js/script.js
+++ js/script.js
@@ -1,159 +1,146 @@
-// function Pizza(size,crust,toppings) {
-//     this.size = size;
-//     this.crust = crust;
-//     this.toppings = toppings;
-// }
-// function Address(street, estate, phoneDetails) {
-//     this.street = street;
-//     this.estate = estate;
-//     this.phoneDetails = phoneDetails;
-// }
-
-// function WholeOrder(priceSize, priceCrust, priceToppings) {
-//     this.priceSize = priceSize;
-//     this.priceCrust = priceCrust;
-//     this.priceToppings = priceToppings;
-// }
-
-// // WholeOrder.prototype.delivery = function () {
-// //     return
-// //     " and the total cost is" +
-// //         (this.priceSize +
-// //             +
-// //             this.priceCrust +
-// //             +
-// //             this.priceToppings)
-
-
-// // };
-// Address.prototype.totalCost = function () {
-//     return this.street + " " + this.estate + " " + this.phoneDetails;
-// };
-
-// // Pizza.prototype.ordered = function () {
-// //     return
-
-// //     this.size +
-// //         this.crust +
-// //         this.toppings + "pizza"
-
-// // };
-
-
-
-
-
-// $(document).ready(function () {
-
-//     $("#checkbox").click(function () {
-//         $("#delivery").show();
-
-//     });
-
-//     $("form#form").submit(function (event) {
-//         event.preventDefault();
-
-//         $("#submit").click(function () {
-
-//             if ($("input[name=small]:checked", "form").val() === "small") {
-//                 var size = $("input[name=size]:checked", "form").val();
-//                 var priceSize = parseInt(300);
-//             } else if ($("input[name=size]:checked", "form").val() === "medium") {
-//                 var size = $("input[name=size]:checked", "form").val();
-//                 var priceSize = parseInt(600);
-//             } else $("input[name=size]:checked", "form").val() === "large";
-//             var size = $("input[name=size]:checked", "form").val();
-//             var priceSize = parseInt(900);
-//             ($('#checkbox').is(':checked'))
-
-
-
-//             if ($("input[name=small]:checked", "form").val() === "crispy") {
-//                 var crust = $("input[name=crust]:checked", "form").val();
-//                 var priceCrust = parseInt(100);
-//             } else if ($("input[name=crust]:checked", "form").val() === "stuffed") {
-//                 var crust = $("input[name=crust]:checked", "form").val();
-//                 var priceCrust = parseInt(100);
-//             } else {
-//                 var crust = $("input[name=crust]:checked", "form").val();
-//                 var priceCrust = parseInt(100);
-//             }
-
-
-
-//             if ($("input[name=small]:checked", "form").val() === "crispy") {
-//                 var toppings = $("input[name=crust]:checked", "form").val();
-//                 var priceToppings = parseInt(200);
-//             } else if ($("input[name=crust]:checked", "form").val() === "stuffed") {
-//                 var toppings = $("input[name=crust]:checked", "form").val();
-//                 var priceToppings = parseInt(200);
-//             } else {
-//                 var toppings = $("input[name=crust]:checked", "form").val();
-
-//                 var priceToppings = parseInt(200);
-//             }
-
-//             // var street = $("input#street").val();
-
-//             // var estate = $("input#estate").val();
-
-//             // var phoneDetails = $("input#phoneDetails").val();
-
-//             var myPizza = new Pizza(size,crust,toppings);
-//             var myOrder = new Order(priceSize, priceCrust, priceToppings);
-//             var myAddress = new Address(street, estate, phoneDetails);
-
-//             // if ($("#checkbox").is(":checked")) {
-//             //     if ($("#street").val() === "") {
-//             //         alert("input your valid street");
-//             //     } else if ($("#estate").val() === "") {
-//             //         alert("input your valid estate");
-//             //     } else if ($("#phoneDetails").val() === "") {
-//             //         alert("input your phone details");
-//             //     } else {
-//             //         alert("order");
-//             //     }
-//             // }
-//             // else {
-//             //     alert("your order is: + wholeOrder");
-
-
-
-//             // }
-//         });
-
-
-//     });
-// });
-
-function Pizza(size, crust, toppings){
+total = 0;
+function Pizza(size, crust, toppings,total) {
     this.size = size;
     this.crust = crust;
     this.toppings = toppings;
+    this.total = total;
 }
 
-Pizza.prototype.ordered = function(){
-    return "Your delivery summary is " + this.size + " " + this.crust + " "+ this.toppings+ " pizza";
+Pizza.prototype.ordered = function () {
+    return "Your delivery summary is " + this.size + " " + this.crust + " " + this.toppings + " pizza";
 }
 
-function Address(street,estate,phoneDetails){
+function Address(street, estate, phoneDetails) {
     this.street = street;
     this.estate = estate;
     this.phoneDetails = phoneDetails;
 }
 
-Address.prototype.addressDetails = function(){
-    return "Your order will be ready in 15mins and will be delivered at " + this.street +", "+this.estate + "and you will be contacted through: "+this.phoneDetails;
-} 
+Address.prototype.addressDetails = function () {
+    return "Your order will be ready in 15mins and will be delivered at " + this.street + ", " + this.estate + "and you will be contacted through: " + this.phoneDetails;
+}
 
-function PriceCalculation(priceOfSize,priceOfCrust,priceOfToppings){
+function PriceCalculation(priceOfSize, priceOfCrust, priceOfToppings) {
     this.priceOfSize = priceOfSize;
     this.priceOfCrust = priceOfCrust;
     this.priceOfToppings = priceOfToppings;
 }
 
-PriceCalculation.prototype.totalCost =  function(){
-    return "Your Order totals " + (this.priceOfSize+this.priceOfCrust+this.priceOfToppings);
+PriceCalculation.prototype.totalCost = function () {
+    return "Your Order totals " + (this.priceOfSize + this.priceOfCrust + this.priceOfToppings);
 }
 
-(document)
+$(document).ready(function () {
+
+    $("#checkbox").click(function () {
+        $("#delivery").show();
+
+    });
+
+    $("button#submit").click(function (event) {
+
+
+        let pSize = $("#size option:selected").val();
+        let pCrust = $("#crust option:selected").val();
+        let pToppings = [];
+
+
+        $.each($("input[name=toppings]:checked"),function(){
+            pToppings.push($(this).val());
+        })
+        // alert(pToppings);
+        switch(pSize) {
+            case "0":
+                price = 0;
+                break;
+            case "Small":
+                price = 300;
+                break;
+            case "Medium":
+                price = 600;
+                break;
+            case "Large":
+                price = 900;
+                break;
+            default:
+                console.log("Error");
+        }
+        alert(price);
+
+        switch (pCrust) {
+            case "0":
+                c_price = 0;
+                break;
+            case "Crispy":
+                c_price = 1300;
+                break;
+            case "Stuffed":
+                c_price = 800;
+                break;
+            case "Gluten-free":
+                c_price = 600;
+                break;
+            default:
+                console.log("Error");
+        }
+        alert(price);
+        topping_value = pToppings.length * 100;
+        alert(topping_value);
+        total = price + c_price + topping_value;
+        alert(total);
+        // if((pSize == "0") && (pCrust == "0")){
+        //     alert("it");
+        // }else{
+        //     alert("do this");
+        // }
+        // alert(c_price);
+        // event.preventDefault();
+        //     if ($("input[name=small]:checked", "form").val() === "small") {
+        //         var size = $("input[name=size]:checked", "form").val();
+        //         var priceOfSize = parseInt(300);
+        //     } else if ($("input[name=size]:checked", "form").val() === "medium") {
+        //         var size = $("input[name=size]:checked", "form").val();
+        //         var priceOfSize = parseInt(600);
+        //     } else{
+        //     var size = $("input[name=size]:checked", "form").val();
+        //     var priceOfSize = parseInt(900);
+        // }
+
+
+
+        // if ($("input[name=small]:checked", "form").val() === "crispy") {
+        //     var crust = $("input[name=crust]:checked", "form").val();
+        //     var priceOfCrust = parseInt(100);
+        // } else if ($("input[name=crust]:checked", "form").val() === "stuffed") {
+        //     var crust = $("input[name=crust]:checked", "form").val();
+        //     var priceOfCrust = parseInt(100);
+        // } else {
+        //     var crust = $("input[name=crust]:checked", "form").val();
+        //     var priceOfCrust = parseInt(100);
+        // }
+
+
+
+        // if ($("input[name=small]:checked", "form").val() === "crispy") {
+        //     var toppings = $("input[name=crust]:checked", "form").val();
+        //     var priceOfToppings = parseInt(200);
+        // } else if ($("input[name=crust]:checked", "form").val() === "stuffed") {
+        //     var toppings = $("input[name=crust]:checked", "form").val();
+        //     var priceOfToppings = parseInt(200);
+        // } else {
+        //     var toppings = $("input[name=crust]:checked", "form").val();
+        //     var priceOfToppings = parseInt(200);
+        // }
+
+        // var street = $("input#street").val();
+        // var estate = $("input#estate").val();
+        // var phoneDetails = $("input#phoneDetails").val();
+
+        // var myOrder = new Pizza(size, crust, toppings);
+        // alert(myOrder);
+        // var myAddress = new Address(street,estate,phoneDetails);
+        // var totalPriceOfOrder = new  PriceCalculation(priceOfSize,PriceOfCrust,priceOfToppings);
+        // alert("me");
+    });
+});
+// });
